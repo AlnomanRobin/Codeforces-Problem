01c74a580009527cd92bfea203a766d562e88f7d
Alnoman Robin <robinalnoman@gmail.com>
2025-12-04 03:17:13 +0600

    136A

---
+++ A. Presents.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> p(n + 1), ans(n + 1);
+
+    for(int i = 1; i <= n; i++) {
+        cin >> p[i];
+        ans[p[i]] = i;  // friend i gives gift to p[i]
+    }
+
+    for(int i = 1; i <= n; i++) {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
+
