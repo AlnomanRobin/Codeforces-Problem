8a743230e112bfdd1f70bee031ff1fb3bf0bbda7
Alnoman Robin <robinalnoman@gmail.com>
2026-02-05 00:00:13 +0600

    Add files via upload

--- A. Expression.cpp
+++ A. Expression.cpp
@@ -1,18 +1,21 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int a, b, c;
     cin >> a >> b >> c;
 
-    int ans1 = a + b + c;
-    int ans2 = a * b * c;
-    int ans3 = (a + b) * c;
-    int ans4 = a * (b + c);
-    int ans5 = a + b * c;
+    int ans = 0;
 
-    cout << max({ans1, ans2, ans3, ans4, ans5}) << endl;
+    ans = max(ans, a + b + c);
+    ans = max(ans, a + b * c);
+    ans = max(ans, a * b + c);
+    ans = max(ans, a * b * c);
+    ans = max(ans, (a + b) * c);
+    ans = max(ans, a * (b + c));
+
+    cout << ans << endl;
 
     return 0;
 }
