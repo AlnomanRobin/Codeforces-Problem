e0e5b84f077e0d0e4bf3244690b190044dd72f8d
Alnoman Robin <robinalnoman@gmail.com>
2026-02-07 16:10:42 +0600

    Add files via upload

---
+++ A. Theatre Square.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n, m, a;
+    cin >> n >> m >> a;
+
+    long long tiles_n = (n + a - 1) / a;
+    long long tiles_m = (m + a - 1) / a;
+
+    cout << tiles_n * tiles_m << endl;
+
+    return 0;
+}
+
