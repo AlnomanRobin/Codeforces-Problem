adf9d40727a15ee3026f7d6bc24a6a6de6a107b4
Alnoman Robin <robinalnoman@gmail.com>
2026-02-11 18:05:08 +0600

    Add files via upload

---
+++ A. Coins.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        long long n, k;
+        cin >> n >> k;
+
+        if(k == 1) {
+            cout << "YES\n";
+        }
+        else if(k % 2 == 0) {
+            if(n % 2 == 0)
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+        else {
+            if(n % 2 == 0)
+                cout << "YES\n";
+            else {
+                if(n >= k)
+                    cout << "YES\n";
+                else
+                    cout << "NO\n";
+            }
+        }
+    }
+
+    return 0;
+}
