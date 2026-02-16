72080d71e9f55a859e6898ef29cf45d51f66e2db
Alnoman Robin <robinalnoman@gmail.com>
2026-02-17 00:55:53 +0600

    Add files via upload

---
+++ YES or YES.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        for (int i = 0; i < 3; i++) {
+            s[i] = toupper(s[i]);
+        }
+
+        if (s == "YES")
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
+
