7bad794233b8bb570e485c588e3d3a9573cfd95e
Alnoman Robin <robinalnoman@gmail.com>
2026-03-04 13:20:49 +0600

    Add files via upload

---
+++ Codeforces Checking.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    string s = "codeforces";
+    while (t--)
+    {
+        char c;
+        cin >> c;
+        if (s.find(c) != string::npos)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
