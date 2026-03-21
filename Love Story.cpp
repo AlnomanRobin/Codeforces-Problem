3d0f46c7d23ce05225ea52ddba56d25a7138ac76
Alnoman Robin <robinalnoman@gmail.com>
2026-03-21 21:19:43 +0600

    Add files via upload

---
+++ Love Story.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+
+    string target = "codeforces";
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        int diff = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            if (s[i] != target[i])
+            {
+                diff++;
+            }
+        }
+
+        cout << diff << "\n";
+    }
+
+    return 0;
+}
