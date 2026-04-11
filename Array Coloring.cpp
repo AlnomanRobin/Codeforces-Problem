a3ab1a0d9569eca12fa9d8529e4b552d77539c3f
Alnoman Robin <robinalnoman@gmail.com>
2026-04-12 00:30:21 +0600

    Add files via upload

--- Array Coloring.cpp
+++ Array Coloring.cpp
@@ -1,42 +1,27 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--)
-        {
+    int t,i,x;
+    cin>>t;
+    while(t--)
+    {
         int n;
-        cin >> n;
-        int odd = 0, even = 0;
-        for (int i = 0; i < n; i++)
+        cin>>n;
+        int odd=0,even=0;
+        for(i=0;i<n;i++)
         {
-            int x;
-            cin >> x;
-            if (x % 2 == 0)
+            cin>>x;
+            if(x%2==0)
                 even++;
             else
                 odd++;
         }
-        if (odd == 0)
-        {
-            cout << "YES\n";
-        }
-        else if (even > 0 && odd > 0)
-        {
-            cout << "YES\n";
-        }
-        else if (odd >= 2 && odd % 2 == 0)
-        {
-            cout << "YES\n";
-        }
+        if(odd>=2 || (odd>=1 && even>=1))
+            cout<<"YES\n";
         else
-        {
-            cout << "NO\n";
-        }
+            cout<<"NO\n";
     }
+
     return 0;
 }
