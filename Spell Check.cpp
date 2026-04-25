c8b5b0fa0d0467198dd4c47b5acecb019c3b77fb
Alnoman Robin <robinalnoman@gmail.com>
2026-04-26 05:23:02 +0600

    Add files via upload

---
+++ Spell Check.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        if (n != 5){
+            cout << "NO\n";
+            continue;
+        }
+        sort(s.begin(),s.end());
+        string target = "Timur";
+        sort(target.begin(), target.end());
+
+        if (s == target)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
+
