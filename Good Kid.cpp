2369b15403e1cdb40f12cee12bcbcc4edc47dc07
Alnoman Robin <robinalnoman@gmail.com>
2026-05-04 15:32:54 +0600

    Add files via upload

---
+++ Good Kid.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        long long ans = 0;
+        for (int i = 0; i < n; i++){
+            a[i]++;
+            long long product = 1;
+            for (int j = 0; j < n; j++){
+                product *= a[j];
+            }
+
+            ans = max(ans, product);
+            a[i]--;
+        }
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
